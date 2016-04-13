be44b6d92195515f565fefd7f87b4543938abe28
Levanevski-Perevozchikov Ilya <ilp416@gmail.com>
2016-04-13 15:35:21 +0300

    Fix aws 3s settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Rails.application.configure do
   # Config for s3 storage
   config.paperclip_defaults = {
     :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
